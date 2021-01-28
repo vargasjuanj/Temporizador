307c6d9034e748f2b0285b9812a32f6bbf91dfdb
vargasjuanj <juanjovargas443377@gmail.com>
2021-01-28 00:54:27 -0300

    con/sin pipe async

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -11,18 +11,23 @@ import { Subscription } from "rxjs/Subscription";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TimerComponent implements OnInit, OnDestroy {
-
   @Output() onComplete = new EventEmitter<string>();
   @Input() init:number = 20;
   private countdownEndRef: Subscription = null;
-test:number=10
+sin_pipe_async:any
+count:number=10;
   constructor(public timer:TimerService){}
 
   ngOnInit(){
-  //el initi toma el valor que se pasa por plantilla desde el appComponent, ignorando el valor del componente, en este caso ignora el 20
+  //el init toma el valor que se pasa por plantilla desde el appComponent, ignorando el valor del componente, en este caso ignora el 20
   this.timer.restartCountdown(this.init);
 
 
+//En ves de usar en la plantilla el pipe "timer.countdown$ | async" nos podemos suscribir de esta forma, pero la ventaja que tiene el pipe que se desuscribe automaticamente
+this.timer.countdown$.subscribe(n=>{
+  this.sin_pipe_async=n // con String Interpolación no refresca en la plantill, usando two data binding con la directiva ngModel si actualiza el valor
+  //alert(n) //asi si lo muestra bien, 10, 9 , 8, etc
+})
   //Proceso paralelo a la linea de arriba
   //cuando la cuenta regresiva finalice se va amitir el string a su componente padre y este va a mostrar el valor del evento con un alert
   this.countdownEndRef = this.timer.countdownEnd$.subscribe(()=>{
