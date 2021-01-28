957cb70d5ae02f8d1abb2aeb3d7dd6a5a9473c53
vargasjuanj <juanjovargas443377@gmail.com>
2021-01-28 00:57:27 -0300

    Ok

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -11,12 +11,15 @@ import { Subscription } from "rxjs/Subscription";
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TimerComponent implements OnInit, OnDestroy {
-  // Hasta ahora la cuenta atrás la cojo directamente en el template del componente, accediendo a timer.countdown.
+  // NOTA=> No funciona lo de ABAJO, pasa por ejemplo del 10 al 0 en la vista, si invocamos un alert en el servicio si muestra el decremento pero en el componente no.
+
+  
+  /* Hasta ahora la cuenta atrás la cojo directamente en el template del componente, accediendo a timer.countdown.
 
   // Esto, no es muy eficiente, ya que es angular quien todo el rato tiene que comprobar si el valor de countdown ha cambiado. Sería mejor que el propio servicio me avisara cuando el valor ha cambiado. 
-  // Esto es lo que se conoce como Programación Reactiva.
+   Esto es lo que se conoce como Programación Reactiva.
+   */
 
-// No funciona lo de arriba, pasa por ejemplo del 10 al 0 en la vista, si invocamos un alert en el servicio si muestra el decremento pero en el componente no.
   @Output() onComplete = new EventEmitter<void>();
   @Input() init:number = 20;
 
@@ -39,36 +42,5 @@ aux:any
     this.countdownEndRef.unsubscribe();
   }
 
-
-//   @Output() onComplete = new EventEmitter<string>();
-//   @Input() init:number = 20;
-//   private countdownEndRef: Subscription = null;
-// aux:any
-// count:number=10;
-//   constructor(public timer:TimerService){}
-
-//   ngOnInit(){
-//   //el initi toma el valor que se pasa por plantilla desde el appComponent, ignorando el valor del componente, en este caso ignora el 20
-//   this.timer.restartCountdown(this.init);
-
-
-// //En ves de usar en la plantilla el pipe "timer.countdown$ | async" nos podemos suscribir de esta forma, pero la ventaja que tiene el pipe que se desuscribe automaticamente
-// this.timer.countdown$.subscribe(n=>{
-//   this.aux=n // con String Interpolación no refresca en la plantill, usando two data binding con la directiva ngModel si actualiza el valor
-//   //alert(n) //asi si lo muestra bien, 10, 9 , 8, etc
-// })
-//   //Proceso paralelo a la linea de arriba
-//   //cuando la cuenta regresiva finalice se va amitir el string a su componente padre y este va a mostrar el valor del evento con un alert
-//   this.countdownEndRef = this.timer.countdownEnd$.subscribe(()=>{
-//     this.onComplete.emit('cuenta regresiva finalizada');
-//   });
-
-
-//   }
-
-//   ngOnDestroy(){
-//     this.timer.destroy();
-//     this.countdownEndRef.unsubscribe();  
-//   }
 
 }
--- src/app/timer/timer.service.ts
+++ src/app/timer/timer.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Subject } from "rxjs/Subject";
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 
 @Injectable()
@@ -32,7 +31,8 @@ export class TimerService {
   private doCountdown(){
     if(this.countdown > 0){
        this.countdownTimerRef = setTimeout(()=>{
-        this.countdown=this.countdown-1;
+        this.countdown--;
+        alert(this.countdown); //Desde aca si muestra el decremento, en el componente no actualiza el valor
         this.processCountdown();
       }, 1000);
     }
@@ -53,64 +53,5 @@ export class TimerService {
     //remove countdown reference
   }
 
-  // private countdownTimerRef:any = null;
-  // private init:number = 0;
-  // private countdownSource = new BehaviorSubject<number>(0);
-  // private countdownEndSource = new Subject<void>();
-  // public countdown$ = this.countdownSource.asObservable();
-  // public countdownEnd$ = this.countdownEndSource.asObservable();
-
-  // constructor() { }
-
-  // destroy():void{
-  //   this.clearTimeout();
-  // }
-
-  // restartCountdown(init?){
-  //   if(init){ // si no es cero, le asignamos el valor del parametro a el init del servicio
-   
-  //     this.init = init;
-  //   }
-
-
-  //   if(this.init >0){
-     
-  //      //this.clearTimeout();
-  //     this.countdownSource.next(this.init); //Si es mayor a cero convertimos el numero en un stream y al ser subjectBehavior mantiene el estado, lo cargamos con cierto numero inicial
-
-  //      this.doCountdown(); //Enpieza a hacer la cuentra regresiva en base a cierto numero
-  //   }
-   
-  // }
-
-  // private doCountdown(){
-  //   if(this.countdownSource.getValue() > 0){  //al usarse recursivamente necesitamos la condición de quiebre, cuando el valor del subjectBehavior llego a cero será el punto de quiebre, si no iremos disminuyendo de a un valor cada un segundo
-  //     this.countdownTimerRef = setTimeout(()=>{
-  //       this.countdownSource.next(this.countdownSource.getValue() -1); //con el next emitimos otro valor, en este caso le restamos uno, como este tipo de observable tiene estado podemos ir restando de a 1
-  //       this.processCountdown(); //validamos si ha terminado o volvemos a llamar a doCountdown
-  //     }, 1000);
-  //   }
-  // }
-
-  // private processCountdown(){
-  //   if(this.countdownSource.getValue() <= 1){
  
-  //     //Cuando emito el stream en el timerComponent al estar suscrito a countdownEnd$, llega este stream y se activa el evenEmitter, que emite el alert desde appComponent, y se deja de emitir el otro flujo del numero para atras, porque no entra en el else ya que se cumple la condición de que si llega a cero, o en este caso que le pusimos 1 para testear
-  //     this.countdownEndSource.next();
-
-      
-  //   }
-  //   else{
-    
-  //     this.doCountdown();
-  //   }
-  // }
-
-  // private clearTimeout(){
-  //   if(this.countdownTimerRef){
-  //     clearTimeout(this.countdownTimerRef);
-  //     this.countdownTimerRef = null;
-  //   }
-  // }
-
 }
